1d73574fa4dc9ea3474c6321f4d8e5a06447c27e
lakki99 <pulka143341@gmail.com>
2025-05-12 20:35:00 +0530

    Update verification.py

--- plugins/verification.py
+++ plugins/verification.py
@@ -165,10 +165,10 @@ def get_readable_time(seconds):
 
 verifydb = VerifyDB()
  
-    '''
+    """
 Note ⚠️: Add This Code In Root Of Your Repo Or Anywhere You Want.
 Credit : @GK-BOTZ
-'''
+"""
 
 
 
